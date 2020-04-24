1620c92ddd2dbd743dcd48f22ee8febbd64afd03
David Vävinggren <61325141+davidvavinggren@users.noreply.github.com>
2020-04-24 13:23:43 +0200

    hämtat relevanta dictionaries

--- project/stock_evaluator.py
+++ project/stock_evaluator.py
@@ -3,27 +3,8 @@ import sys
 import common
 import matplotlib
 import matplotlib.pyplot as plt
-import yfinance as yf
 
-
-API_SERVER = "https://financialmodelingprep.com/api/v3/"
-
-#Dessa dictionaries tror jag kan bli användbara:
-
-#https://financialmodelingprep.com/api/v3/financials/income-statement/AAPL
-#https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/AAPL
-#https://financialmodelingprep.com/api/v3/financial-ratios/AAPL
-#https://financialmodelingprep.com/api/v3/company/profile/AAPL
-#https://financialmodelingprep.com/api/v3/quote/AAPL,FB
-#https://financialmodelingprep.com/api/v3/company-key-metrics/AAPL?period=quarter
-#https://financialmodelingprep.com/api/v3/company/rating/AAPL
-#https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?serietype=line
-
-def get_jsn_as_dict(stock_label):
-    url = (API_SERVER
-           + "historical-price-full/"
-           + stock_label
-           + "?serietype=line")
+def get_jsn_as_dict(url):
     uerel = requests.get(url)
     uerel_json = uerel.json()
     if uerel.status_code == 200:
@@ -46,7 +27,6 @@ def plot_graph(list, company_name, plot_name = 0, plot_title = 0,
                currency = 0, x_label = 0, y_label = 0):
     x_values = []
     y_values = []
-    #step_length =
     for element in list:
         x_values.append(element["date"][2:4])
         y_values.append(element["close"])
@@ -58,25 +38,42 @@ def plot_graph(list, company_name, plot_name = 0, plot_title = 0,
     plt.xlabel(x_label)
     plt.savefig(plot_name)
 
+def get_data(category, label):
+    API_SERVER = "https://financialmodelingprep.com/api/v3/"
+    url_dict = {"income" : "financials/income-statement/{}".format(label),
+                "assets" : "financials/balance-sheet-statement/{}".format(label),
+                "financial_ratios" : "financial-ratios/{}".format(label),
+                "info" : "company/profile/{}".format(label),
+                "key_metrics" : "company-key-metrics/{}?period=quarter".format(label),
+                "rating" : "company/rating/{}".format(label),
+                "price" : "historical-price-full/{}?serietype=line".format(label)}
+    url = API_SERVER + url_dict[category]
 
-def Main(label):
-    currency = "USD"
-    company = yf.Ticker(label)
-    company_name = company.info["longName"]
-    stock_label = label.upper()
-    if get_jsn_as_dict(stock_label):
-        f_history = get_jsn_as_dict(stock_label)
+    if get_jsn_as_dict(url):
+        dict = get_jsn_as_dict(url)
     else:
         print("Stock label not found. Please try again. ")
-        return
-    period_annual = get_period_annual(f_history)
+    return dict
+
+
+def Main(label):
+    f_dicts = {"price" : get_data("price", label),
+               "income" : get_data("income", label),
+               "assets" : get_data("assets", label),
+               "financial_ratios" : get_data("financial_ratios", label),
+               "info" : get_data("info", label),
+               "key_metrics" : get_data("key_metrics", label),
+               "rating" : get_data("rating", label)}
+    f_data = {""}
+    currency = "USD"
+    company_name = f_dicts["info"]["profile"]["companyName"]
+    label = label.upper()
+    period_annual = get_period_annual(f_dicts["price"])
     plot_graph(period_annual,
                company_name,
-               "annual_closing_price.png",
-               " annual closing price",
+               "annual_stock_price.png",
+               " annual stock price",
                "USD", "Time [year]",
                "Value [" + currency + "]")
-
-
 
 Main(sys.argv[1])
