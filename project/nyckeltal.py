fb23b9430df54a6f4702d9118a2d3cff0c2e7abb
emisa182 <55809352+emisa182@users.noreply.github.com>
2020-07-28 13:00:19 +0200

    Create nyckeltal.py
    Skiss på att lagra nyckeltal

---
+++ project/nyckeltal.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Klass av nyckeltal.
+
+Filen ska kunna användas som hjälp för att importera nyckeltal, kan eventuellt
+läggas till en senare fil som en enda klass.
+"""
+
+class Nyckeltal(object):
+    """Fäst nyckeltal till ett bolag som representerar klassen"""
+
+    def __init__(self, name, price, earnings, amount_of_stocks):
+        """Tar in bolagsdata"""
+        self.name = name
+        self.pe = (price*amount_of_stocks)/earnings
+        self.peg = self.pe/growth
