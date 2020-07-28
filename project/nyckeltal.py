10991588fa951c243583d74a71b9271dfef725c3
emisa182 <55809352+emisa182@users.noreply.github.com>
2020-07-28 13:04:08 +0200

    Update nyckeltal.py

--- project/nyckeltal.py
+++ project/nyckeltal.py
@@ -5,11 +5,13 @@ Filen ska kunna användas som hjälp för att importera nyckeltal, kan eventuell
 läggas till en senare fil som en enda klass.
 """
 
+#osäker om klasser eller dictionaries är lämpligste sättet att lagra datan
+
 class Nyckeltal(object):
     """Fäst nyckeltal till ett bolag som representerar klassen"""
 
-    def __init__(self, name, price, earnings, amount_of_stocks):
+    def __init__(self, name, price, earnings, amount_of_stocks, earning_growth):
         """Tar in bolagsdata"""
         self.name = name
         self.pe = (price*amount_of_stocks)/earnings
-        self.peg = self.pe/growth
+        self.peg = self.pe/earning_growth
